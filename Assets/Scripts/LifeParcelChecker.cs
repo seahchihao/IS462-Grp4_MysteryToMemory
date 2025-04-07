f557db17e3c41eb22f786da9d4dbada05511ac49
lzywil <wilfredlimziyuan@gmail.com>
2025-04-07 11:52:17 +0800

    Adulthood

---
+++ Assets/Scripts/LifeParcelChecker.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class LifeParcelChecker : MonoBehaviour
+{
+    public AudioClip deliverySound;
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+        if (audioSource == null)
+        {
+            // Auto-add AudioSource if missing
+            audioSource = gameObject.AddComponent<AudioSource>();
+            audioSource.playOnAwake = false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("LifeParcel"))
+        {
+            // Deactivate the parcel
+            other.gameObject.SetActive(false);
+
+            // Play the delivery sound at the delivery zone
+            if (deliverySound != null)
+            {
+                audioSource.PlayOneShot(deliverySound);
+            }
+
+            Debug.Log("Parcel delivered!");
+        }
+    }
+}
